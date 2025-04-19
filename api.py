93f9270c7f459047b0eaa4691fd175fb38bf4b91
Vojtech Kloda <vojtech.kloda@gmail.com>
2025-04-19 09:23:03 +0200

    fix urllib capitalization issue

--- api.py
+++ api.py
@@ -15,7 +15,7 @@ from src.recodex_cli_lib.generated.swagger_client import DebugParamBody
 from src.recodex_cli_lib.endpoint_resolver import EndpointResolver
 
 
-token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3lvdXIucmVjb2RleC5kb21haW4iLCJhdWQiOiJodHRwczovL3lvdXIucmVjb2RleC5kb21haW4iLCJpYXQiOjE3NDE4NzE3NzEsIm5iZiI6MTc0MTg3MTc3MSwiZXhwIjoxNzQyNDc2NTcxLCJzdWIiOiJiNjA1MGFhNC1mODExLTQxMDItYWU4NC0wN2MzOTAwNmFkNWMiLCJlZmZyb2xlIjpudWxsLCJzY29wZXMiOlsibWFzdGVyIiwicmVmcmVzaCJdfQ.9rQYUhZmeze2CpuZ12pDt1xM3WlompU1Gvxiiyd0c_E"
+token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3lvdXIucmVjb2RleC5kb21haW4iLCJhdWQiOiJodHRwczovL3lvdXIucmVjb2RleC5kb21haW4iLCJpYXQiOjE3NDQ4OTQ5MTcsIm5iZiI6MTc0NDg5NDkxNywiZXhwIjoxNzQ1NDk5NzE3LCJzdWIiOiJiNjA1MGFhNC1mODExLTQxMDItYWU4NC0wN2MzOTAwNmFkNWMiLCJlZmZyb2xlIjpudWxsLCJzY29wZXMiOlsibWFzdGVyIiwicmVmcmVzaCJdfQ.xx5z3Lnq2oCCfbhMdT2XL2_MsDV2dfQbyp4iQXhJPfk"
 host = "http://localhost:4000"
 
 client = Client(token, host)
@@ -26,7 +26,7 @@ body = DebugParamBody(
 )
 
 path_params = { "param":"1" }
-query_params = { "b": False, "c": 5.5 }
+query_params = { "b": True, "c": 5.5 }
 
 response = client.send_request('reg', 'dbg', body, path_params, query_params)
 print(response.data)
--- src/recodex_cli_lib/client.py
+++ src/recodex_cli_lib/client.py
@@ -5,7 +5,7 @@ from .generated.swagger_client.configuration import Configuration
 from .generated.swagger_client.rest import ApiException
 from .generated.swagger_client.rest import RESTResponse
 from .swagger_validator import SwaggerValidator
-from.endpoint_resolver import EndpointResolver
+from .endpoint_resolver import EndpointResolver
 
 class Client:
     def __init__(self, token, host):
@@ -16,11 +16,22 @@ class Client:
         self.endpoint_resolver = EndpointResolver(self.generated_api)
         self.validator = SwaggerValidator(self.endpoint_resolver)
 
+    # converts boolean values of the dict to 'true' or 'false'
+    # the urllib.urlencode function used converts bools to 'True' or 'False', which causes an error on the endpoint
+    def fix_boolean_url_params(self, params: dict):
+        for key, value in params.items():
+            if value == True or value == False:
+                params[key] = str(value).lower()
+
     def send_request(self, presenter, endpoint, body={}, path_params={}, query_params={}):
         endpoint_callback = self.endpoint_resolver.get_endpoint_callback(presenter, endpoint)
 
         # validate the request (throws jsonschema.exceptions.ValidationError when invalid)
         self.validator.validate(presenter, endpoint, body, path_params, query_params)
+
+        # convert boolean values to strings to avoid urllib errors
+        self.fix_boolean_url_params(path_params)
+        self.fix_boolean_url_params(query_params)
 
         endpoint_callback(body=body, **path_params, **query_params)
         response: RESTResponse = self.generated_client.last_response
