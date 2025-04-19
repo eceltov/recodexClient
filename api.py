7c98341595cd7a9434c664fa437cf7676b659d93
Vojtech Kloda <vojtech.kloda@gmail.com>
2025-04-19 15:33:57 +0200

    removed unused imports

--- api.py
+++ api.py
@@ -1,19 +1,4 @@
-from __future__ import print_function
-import time
-# import src.generated.swagger_client as swagger_client
-# from src.client import Client
-
 from src.recodex_cli_lib.client import Client
-from src.recodex_cli_lib.swagger_validator import SwaggerValidator
-
-# import swagger_client
-from src.recodex_cli_lib.generated.swagger_client.rest import ApiException
-from src.recodex_cli_lib.generated.swagger_client.configuration import Configuration
-from src.recodex_cli_lib.generated.swagger_client.rest import RESTResponse
-from src.recodex_cli_lib.generated.swagger_client import DebugParamBody
-
-from src.recodex_cli_lib.endpoint_resolver import EndpointResolver
-
 
 token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3lvdXIucmVjb2RleC5kb21haW4iLCJhdWQiOiJodHRwczovL3lvdXIucmVjb2RleC5kb21haW4iLCJpYXQiOjE3NDQ4OTQ5MTcsIm5iZiI6MTc0NDg5NDkxNywiZXhwIjoxNzQ1NDk5NzE3LCJzdWIiOiJiNjA1MGFhNC1mODExLTQxMDItYWU4NC0wN2MzOTAwNmFkNWMiLCJlZmZyb2xlIjpudWxsLCJzY29wZXMiOlsibWFzdGVyIiwicmVmcmVzaCJdfQ.xx5z3Lnq2oCCfbhMdT2XL2_MsDV2dfQbyp4iQXhJPfk"
 host = "http://localhost:4000"
--- apiPackageTest.py
+++ apiPackageTest.py
@@ -1,30 +1,15 @@
-from __future__ import print_function
-import time
-# import generated.swagger_client as swagger_client
-# from client import Client
-
 from recodex_cli_lib.client import Client
-from recodex_cli_lib.swagger_validator import SwaggerValidator
-
-# import swagger_client
-from recodex_cli_lib.generated.swagger_client.rest import ApiException
-from recodex_cli_lib.generated.swagger_client.configuration import Configuration
-from recodex_cli_lib.generated.swagger_client.rest import RESTResponse
-from recodex_cli_lib.generated.swagger_client import DebugParamBody
-
-from recodex_cli_lib.endpoint_resolver import EndpointResolver
-
 
 token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3lvdXIucmVjb2RleC5kb21haW4iLCJhdWQiOiJodHRwczovL3lvdXIucmVjb2RleC5kb21haW4iLCJpYXQiOjE3NDQ4OTQ5MTcsIm5iZiI6MTc0NDg5NDkxNywiZXhwIjoxNzQ1NDk5NzE3LCJzdWIiOiJiNjA1MGFhNC1mODExLTQxMDItYWU4NC0wN2MzOTAwNmFkNWMiLCJlZmZyb2xlIjpudWxsLCJzY29wZXMiOlsibWFzdGVyIiwicmVmcmVzaCJdfQ.xx5z3Lnq2oCCfbhMdT2XL2_MsDV2dfQbyp4iQXhJPfk"
 host = "http://localhost:4000"
 
 client = Client(token, host)
 
-body = DebugParamBody(
-    a="tes",
-    d=7.1,
-    e=False
-)
+body = {
+    "a": "tes",
+    "d": 7.1,
+    "e": False
+}
 
 path_params = { "param":"1" }
 query_params = { "b": True, "c": 5.5 }
