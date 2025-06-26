be2a94303d7838dfb7e6b73da42904581adef99c
Vojtech Kloda <vojtech.kloda@gmail.com>
2025-06-26 13:05:09 +0200

    made client response getters throw better error messages

--- src/recodex_cli_lib/client_response.py
+++ src/recodex_cli_lib/client_response.py
@@ -30,9 +30,15 @@ class ClientResponse():
     def get_json_string(self, minimized: bool = False) -> str:
         if minimized:
             return self.data
-        return json.dumps(self.__get_parsed_data_or_throw(), indent=2, ensure_ascii=False)
+        try:
+            return json.dumps(self.__get_parsed_data_or_throw(), indent=2, ensure_ascii=False)
+        except:
+            raise Exception("The response data is not in JSON format.")
     
     def get_yaml_string(self, minimized: bool = False) -> str:
-        if minimized:
-            return yaml.dump(self.__get_parsed_data_or_throw(), default_flow_style=True, indent=None, allow_unicode=True)
-        return yaml.dump(self.__get_parsed_data_or_throw(), allow_unicode=True, indent=2)
+        try:
+            if minimized:
+                return yaml.dump(self.__get_parsed_data_or_throw(), default_flow_style=True, indent=None, allow_unicode=True)
+            return yaml.dump(self.__get_parsed_data_or_throw(), allow_unicode=True, indent=2)
+        except:
+            raise Exception("The response data could not be converted to YAML.")
