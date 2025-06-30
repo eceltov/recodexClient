60b55cb845c37fd04e137f87a6af8ec51f270c6b
Vojtech Kloda <vojtech.kloda@gmail.com>
2025-06-30 19:36:11 +0200

    failed tests should fail CI/CD now

--- tests/testClasses/__main__.py
+++ tests/testClasses/__main__.py
@@ -1,6 +1,8 @@
+import unittest
+import sys
+
 from .request_tests import RequestTests
 from .validation_tests import ValidationTests
-import unittest
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
@@ -11,4 +13,9 @@ suite.addTests(loader.loadTestsFromTestCase(ValidationTests)) # Add other test c
 
 # run the tests
 runner = unittest.TextTestRunner(verbosity=2)
-runner.run(suite)
+result = runner.run(suite)
+
+# return error code 1 on failure
+if not result.wasSuccessful():
+    sys.exit(1)
+sys.exit(0)
