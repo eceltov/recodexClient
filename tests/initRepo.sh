d21dd2dbcf7d57cc82886e15fc6baf1ae501a097
Vojtech Kloda <vojtech.kloda@gmail.com>
2025-06-30 15:57:16 +0200

    changed codegen jar

--- tests/initRepo.sh
+++ tests/initRepo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# path to swagger-codegen
-swaggerCodegenPath=./swagger-codegen-cli-3.0.9.jar
+# path to swagger-codegen (https://github.com/swagger-api/swagger-codegen/tree/3.0.0)
+swaggerCodegenPath=./swagger-codegen
 
 # path to the new swagger specification file from the ReCodEx api repository
 recodexSwaggerDocsPath=./tests/swagger.yaml
@@ -10,13 +10,16 @@ recodexSwaggerDocsPath=./tests/swagger.yaml
 generatedPath=./src/recodex_cli_lib/generated
 
 # donwload swagger-codegen
-wget https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.9/swagger-codegen-cli-3.0.9.jar
+git clone https://github.com/swagger-api/swagger-codegen.git
+cd $swaggerCodegenPath
+git checkout fd6f4216b
+cd ..
 
 echo "Removing old generated code"
 rm -r $generatedPath
 
 echo "Generating new client code"
-java -jar "$swaggerCodegenPath" generate \
+java -jar "$swaggerCodegenPath/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar" generate \
    -i $recodexSwaggerDocsPath \
    -l python \
    -o $generatedPath
@@ -34,4 +37,4 @@ sed -i 's/\bswagger_client\.models\b/models/g' src/recodex_cli_lib/generated/swa
 sed -i 's/\bswagger_client\b/..swagger_client/g' src/recodex_cli_lib/generated/swagger_client/api_client.py
 sed -i 's/\bswagger_client\b/...swagger_client/g' src/recodex_cli_lib/generated/swagger_client/api/__init__.py
 sed -i 's/\bswagger_client\b/...swagger_client/g' src/recodex_cli_lib/generated/swagger_client/api/default_api.py
-sed -i 's/\bswagger_client\b/...swagger_client/g' src/recodex_cli_lib/generated/swagger_client/models/*.py
+sed -i 's/\bswagger_client\b/...swagger_client/g' src/recodex_cli_lib/generated/swagger_client/models/__init__.py
